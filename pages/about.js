82c06114bd930b7ff4489174011e1f6b671b5b4f
vikram <vikramandanshu@gmail.com>
2022-04-18 15:18:56 +0530

    index pg

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>about me page</div>
+  )
+}
+
+export default about    
---
+++ pages/blogpost/keyboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const keyboard = () => {
+  return (
+    <div>how to use keyboards</div>
+  )
+}
+
+export default keyboard
