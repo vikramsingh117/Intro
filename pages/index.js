e72d416f92f4efcdbb48b1c752434bb13e607f05
vikram <vikramandanshu@gmail.com>
2022-04-18 15:49:25 +0530

    index

--- pages/index.js
+++ pages/index.js
@@ -51,14 +51,14 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://drive.google.com/file/d/1c_NpwLBqdzDG9DrzwkX_F4JWnIhBAs5U/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
+          Powered by me, just me.
+          {/* <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          </span> */}
         </a>
       </footer>
     </div>
