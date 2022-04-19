35619c4f64f758f10d5060d689db5c42d6f62994
vikram <vikramandanshu@gmail.com>
2022-04-19 21:50:05 +0530

    about2

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,12 +1,10 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-const slug = () => {
-  const router = useRouter()
-  const { Slug } = router.query
-  
-  return (
-    <div>{Slug}</div>
-  )
-}
+import React from "react";
+import { useRouter } from "next/router";
+const Slug = () => {
+  const router = useRouter();
+  const { Slug } = router.query;
 
-export default slug
+  return <div>{Slug}</div>;
+};
+
+export default Slug;
