dc88d9597fd96d0315a0aa6270d21b912cc4e0cf
vikramsingh117 <vikramandanshu@gmail.com>
2025-01-04 20:42:00 +0530

    added project cards

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,37 @@ export default function Home() {
           </Head>
 
           <main className={styles.main}>
+
+            {/* Scattered Project Cards */}
+
+            <div className={styles.projectCards}>
+              <div className={`${styles.projectCard} ${styles.compilerCard}`}>
+                <h3>Simplified Compiler</h3>
+                <p>Technology: C, LEX, YACC</p>
+                <p className={styles.projectDescription}>
+                  A compiler that parses and compiles 200 lines of code.
+                </p>
+              </div>
+
+              <div className={`${styles.projectCard} ${styles.figmaCard}`}>
+                <h3>Figma Clone</h3>
+                <p>Technology: Next.js, Codeblocks</p>
+                <p className={styles.projectDescription}>
+                  Implemented live collaboration features and improved SEO.
+                </p>
+              </div>
+
+              <div className={`${styles.projectCard} ${styles.bwappCard}`}>
+                <h3>bWAPP Attack</h3>
+                <p>Techniques: SQL Injection, Phishing</p>
+                <p className={styles.projectDescription}>
+                  Accessed unprotected CCTV cameras and exploited
+                  vulnerabilities.
+                </p>
+              </div>
+            </div>
+
+
             <h1 className={styles.title}>Welcome to My Website</h1>
 
             <div className={styles.grid}>
@@ -46,7 +77,10 @@ export default function Home() {
                 <h2>?</h2>
               </a>
 
-              <a href="https://github.com/vikramsingh117" className={styles.card1}>
+              <a
+                href="https://github.com/vikramsingh117"
+                className={styles.card1}
+              >
                 <h2>GitHub</h2>
               </a>
             </div>
